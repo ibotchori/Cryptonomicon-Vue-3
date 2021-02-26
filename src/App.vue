5ace549c409678a0e0d73c1efb9ae0499c25bd5b
koregeo <koregm@gmail.com>
2021-02-26 18:43:51 +0400

    select element for view graphic

--- src/App.vue
+++ src/App.vue
@@ -49,6 +49,8 @@
         <div
         v-for="t in tickers"
         :key="t.name"
+        @click="sel = t"
+        :class="sel === t ? 'border-4' : ''"
           class="bg-white overflow-hidden shadow rounded-lg border-purple-800 border-solid cursor-pointer"
         >
           <div class="px-4 py-5 sm:p-6 text-center">
@@ -61,7 +63,7 @@
           </div>
           <div class="w-full border-t border-gray-200"></div>
           <button
-          @click="handleDelete(t)"
+          @click.stop="handleDelete(t)"
             class="flex items-center justify-center font-medium w-full bg-gray-100 px-4 py-4 sm:px-6 text-md text-gray-500 hover:text-gray-600 hover:bg-gray-200 hover:opacity-20 transition-all focus:outline-none"
           >
             <svg
@@ -83,9 +85,9 @@
       <hr class="w-full border-t border-gray-600 my-4" />
       </template>
 
-      <section class="relative">
+      <section v-if="sel" class="relative" >
         <h3 class="text-lg leading-6 font-medium text-gray-900 my-8">
-          VUE - USD
+          {{sel.name}} - USD
         </h3>
         <div class="flex items-end border-gray-600 border-b border-l h-64">
           <div class="bg-purple-800 border w-10 h-24"></div>
@@ -93,7 +95,7 @@
           <div class="bg-purple-800 border w-10 h-48"></div>
           <div class="bg-purple-800 border w-10 h-16"></div>
         </div>
-        <button type="button" class="absolute top-0 right-0">
+        <button @click="sel = null" type="button" class="absolute top-0 right-0">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -133,7 +135,8 @@ export default {
         {name: "demo2", price: "2"},
         {name: "demo3", price: "3"}
 
-        ]
+        ],
+        sel: null
     };
   },
 
